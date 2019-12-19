7891c0b5efa2d31b364f93d1dfdcb5e816f60f3e
dwane richards <dwanerich@gmail.com>
2019-12-19 00:55:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,9 +12,9 @@ end
 def shipping_manifest
  
  the_manifest = {
-   "whale bone corsets" => 5,
-   "porcelain vases" => 2,
-   "oil paintings" => 3
+             "whale bone corsets" => 5,
+             "porcelain vases" => 2,
+             "oil paintings" => 3
  }
 
 end
@@ -26,7 +26,6 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 shipping_manifest["oil paintings"]
 end
 
@@ -38,10 +37,6 @@ def adding
                  }
 
 shipping_manifest["muskets"] = 2
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
 
 shipping_manifest["gun powder"] = 4
 
